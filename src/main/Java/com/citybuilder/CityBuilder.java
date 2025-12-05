78270146a432c707705b930691e6dfc3c76254db
7Zeb <66693249+7zeb@users.noreply.github.com>
2025-12-05 12:14:12 -0600

    Add main plugin class and command registration
    Implemented the main plugin class for CityBuilder with command registration.

--- src/main/Java/com/citybuilder/CityBuilder.java
+++ src/main/Java/com/citybuilder/CityBuilder.java
@@ -1,1 +1,27 @@
-//coming soon
+package com.citybuilder;
+
+import org.bukkit.plugin.java.JavaPlugin;
+import com.citybuilder.commands.FillerCommand;
+
+public class CityBuilder extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Log startup
+        getLogger().info("CityBuilder has been enabled!");
+
+        // Register commands
+        if (getCommand("fillerbuilding") != null) {
+            getCommand("fillerbuilding").setExecutor(new FillerCommand());
+        }
+
+        // Future: initialize structure manager, load templates, etc.
+        // StructureManager.init(this);
+    }
+
+    @Override
+    public void onDisable() {
+        // Log shutdown
+        getLogger().info("CityBuilder has been disabled!");
+    }
+}
